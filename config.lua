0f0381fc6d2615c0e98c3e85adddf3eabf6f719a
Arman <thisiisarman@gmail.com>
2016-03-15 14:31:15 +0330

    minor bug fixed

--- config.lua
+++ config.lua
@@ -1,30 +1,82 @@
 return {
+	bot_api_key = '', --token
 	bot_api_key = '',
+	google_api_key = '',
+	google_cse_key = '',
+	lastfm_api_key = '',
+	owm_api_key = '',
+	biblia_api_key = '',
+	thecatapi_key = '',
+	nasa_api_key = '',
+	yandex_key = '',
+	simsimi_key = '',
+	simsimi_trial = true,
 	time_offset = 0,
+	lang = 'en',
+	-- If you change this, make sure you also modify launch-tg.sh.
 	cli_port = 4567,
+	admin = 102490691,
+	admin_name = 'John Smith',
+	log_chat = nil,
 	admin = 00000000,
-	admin_name = 'YOU' ,
+	admin_name = 'John Smith',
+	log_chat = nil,
+	about_text = [[
 
+]]	,
 	errors = {
 		connection = 'Connection error.',
 		results = 'No results found.',
 		argument = 'Invalid argument.',
 		syntax = 'Invalid syntax.',
-		not_admin = 'ERR! NOT ADMIN.'
+		chatter_connection = 'I don\'t feel like talking right now.',
+		chatter_response = 'I don\'t know what to say to that.'
+	},
+	greetings = {
+		['Hello, #NAME.'] = {
+			'hello',
+			'hey',
+			'sup',
+			'hi',
+			'good morning',
+			'good day',
+			'good afternoon',
+			'good evening'
+		},
+		['Goodbye, #NAME.'] = {
+			'bye',
+			'later',
+			'see ya',
+			'good night'
+		},
+		['Welcome back, #NAME.'] = {
+			'i\'m home',
+			'i\'m back'
+		},
+		['You\'re welcome, #NAME.'] = {
+			'thanks',
+			'thank you'
+		}
 	},
 	moderation = {
 		admins = {
-			['000000000'] = 'You'
+			['102490691'] = 'Arman'
+
+			['00000000'] = 'You'
 		},
-				realms = {
-			['00000000'] = 'MyRealm! '
+		errors = {
+			antisquig = 'This group is English-only.',
+			moderation = 'I do not moderate this group.',
+			not_mod = 'This command must be run by a moderator.',
+			not_admin = 'This command must be run by an administrator.',
 		},
-		admin_group = 00000000,
-		realm_name = 'MyRealm'
+		admin_group = -00000000,
+		realm_name = 'My Realm',
+		antisquig = false
 	},
 	plugins = {
+		'Block.lua',
 		'Help.lua',
-		'Pv.lua',
-		'Block.lua'
+		'PV.lua'
 	}
 }
